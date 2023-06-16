44536656c6dbb53a6e10073c09c60282850fec0d
Andrey Hoderev <HoderevAV@gmail.com>
2023-06-17 02:40:46 +0400

    Changed audio pesenter

--- Assets/Sources/Presenter/AudioPresenter.cs
+++ Assets/Sources/Presenter/AudioPresenter.cs
@@ -1,18 +1,45 @@
 using CrazyRacing.Model;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioPresenter : MonoBehaviour
 {
+    [SerializeField] private Button _button;
+    [SerializeField] private Image _image;
+    [SerializeField] private Sprite _iconEnabled;
+    [SerializeField] private Sprite _iconDisabled;
+
     private void Awake()
     {
         if (Audio.IsEnabled)
+        {
             Audio.Enable();
+            _image.sprite = _iconEnabled;
+        }
         else
+        {
             Audio.Disable();
+            _image.sprite = _iconDisabled;
+        }
     }
 
-    public void Switch()
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(OnClick);
+    }
+
+    private void OnDisable()
+    {
+        _button.onClick.RemoveListener(OnClick);
+    }
+
+    public void OnClick()
     {
         Audio.Switch();
+
+        if (Audio.IsEnabled)
+            _image.sprite = _iconEnabled;
+        else
+            _image.sprite = _iconDisabled;
     }
 }
